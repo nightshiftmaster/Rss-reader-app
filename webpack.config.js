16dc7d1a4e5d41cbac0554414e09133fbe922905
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2022-05-10 20:52:49 +0300

    webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './index.html',
+      template: path.resolve(__dirname, 'index.html'),
     }),
     new CleanWebpackPlugin(),
   ],
