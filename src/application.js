fe5abbefc2ada5f6128d6ad13f7cb5f0beb1bd56
nightshiftmaster <nightshiftmaster@Nights-MacBook-Air.local>
2022-06-07 15:23:52 +0300

    app update4

--- src/application.js
+++ src/application.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import i18n from 'i18next';
 import resources from './locales/index';
 import initView from './view';
-import processData from './getData';
+import processData from './tools/getData';
 import validate from './tools/validator';
 
 export default () => {
